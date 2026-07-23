c930b2f58d8292a389b6ca851024b65432b781a1
Mohammad Shafiee <muhammad.shafiee@gmail.com>
2026-07-24 00:48:04 +0200

    fix: use virtio-sound-pci + coreaudio audiodev on aarch64 virt

--- qemu-arm64.sh
+++ qemu-arm64.sh
@@ -16,7 +16,8 @@ sudo qemu-system-aarch64 \
   -device usb-ehci,id=usb \
   -device usb-kbd \
   -device usb-tablet \
-  -audio coreaudio \
+  -audiodev coreaudio,id=audio0 \
+  -device virtio-sound-pci,audiodev=audio0 \
   -netdev vmnet-shared,id=net0 \
   -device virtio-net-pci,netdev=net0 \
   -drive file=/Users/mohammad/Downloads/zurvan-live-iso-arm64/live-image-arm64.hybrid.iso,if=virtio,format=raw,readonly=on \
