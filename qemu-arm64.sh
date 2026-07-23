97172df89cd939bc46a16c74a234efabecfb6440
Mohammad Shafiee <muhammad.shafiee@gmail.com>
2026-07-24 00:41:07 +0200

    feat: add CoreAudio backend to qemu-arm64.sh for ARM64 VM audio support

---
+++ qemu-arm64.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+VARS_FILE="/tmp/edk2-arm-vars.fd"
+rm -f "$VARS_FILE"
+cp /opt/homebrew/share/qemu/edk2-arm-vars.fd "$VARS_FILE"
+
+sudo qemu-system-aarch64 \
+  -accel hvf \
+  -cpu host \
+  -smp 4 \
+  -m 4G \
+  -M virt,highmem=on \
+  -drive if=pflash,format=raw,readonly=on,file=/opt/homebrew/share/qemu/edk2-aarch64-code.fd \
+  -drive if=pflash,format=raw,file="$VARS_FILE" \
+  -device virtio-gpu-pci,xres=1920,yres=1080 \
+  -device usb-ehci,id=usb \
+  -device usb-kbd \
+  -device usb-tablet \
+  -audiodev coreaudio,id=audio0 \
+  -device intel-hda,id=sound0,bus=pcie.0,addr=0x4 \
+  -device hda-duplex,id=output0,bus=sound0.0 \
+  -netdev vmnet-shared,id=net0 \
+  -device virtio-net-pci,netdev=net0 \
+  -drive file=/Users/mohammad/Downloads/zurvan-live-iso-arm64/live-image-arm64.hybrid.iso,if=virtio,format=raw,readonly=on \
+  -display cocoa,zoom-to-fit=on
