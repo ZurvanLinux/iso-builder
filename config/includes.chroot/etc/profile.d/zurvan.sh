f09765f1855858ee3e3275b92311e95f712eba88
Mohammad Shafiee <muhammad.shafiee@gmail.com>
2026-07-24 10:16:00 +0200

    fix: load Zurvan environment branding dynamically from /etc/os-release instead of hardcoding

--- config/includes.chroot/etc/profile.d/zurvan.sh
+++ config/includes.chroot/etc/profile.d/zurvan.sh
@@ -1,5 +1,8 @@
-# Zurvan Linux environment branding
-export DISTRIB_ID=Zurvan
-export DISTRIB_RELEASE=0.1.0
-export DISTRIB_CODENAME=akaran
-export DISTRIB_DESCRIPTION="Zurvan Linux 0.1.0"
+# Zurvan Linux environment branding sourced dynamically from /etc/os-release
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    export DISTRIB_ID="${ID:-Zurvan}"
+    export DISTRIB_RELEASE="${VERSION_ID:-0.1.0}"
+    export DISTRIB_CODENAME="${VERSION_CODENAME:-akaran}"
+    export DISTRIB_DESCRIPTION="${PRETTY_NAME:-Zurvan Linux}"
+fi
